45e01a3bdefaa618b8e40c39c07b49bf0a092a6e
foresitegroup <mark@foresitegrp.com>
2016-08-22 14:53:36 -0500

    2016-08-22

--- tour.php
+++ tour.php
@@ -14,7 +14,93 @@ include "header.php";
 </div>
 
 <div class="site-width tour">
-  TOUR CONTENT HERE
+  <ul class="tour-menu">
+    <li><a href="#infant-toddler">INFANT/TODDLER</a></li>
+    <li><a href="#pssa">PRE-SCHOOL &amp; SCHOOL-AGE</a></li>
+    <li><a href="#outdoor">OUTDOOR/FACILITY</a></li>
+  </ul>
+
+  <br><br>
+  <hr><br>
+  <br><br>
+
+  <h3 id="infant-toddler">INFANT AREA/TODDLER AREA</h3>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
+  <br>
+
+  <div class="video" data-id="T3s7bcOmHpg"></div>
+
+  <div class="gallery">
+    <a href="images/tour/infant-toddler01.jpg" class="swipebox" style="background-image: url(images/tour/infant-toddler01.jpg)" rel="it"></a>
+    <a href="images/tour/infant-toddler02.jpg" class="swipebox" style="background-image: url(images/tour/infant-toddler02.jpg)" rel="it"></a>
+    <a href="images/tour/infant-toddler03.jpg" class="swipebox" style="background-image: url(images/tour/infant-toddler03.jpg)" rel="it"></a>
+    <a href="images/tour/infant-toddler04.jpg" class="swipebox" style="background-image: url(images/tour/infant-toddler04.jpg)" rel="it"></a>
+    <a href="images/tour/infant-toddler05.jpg" class="swipebox" style="background-image: url(images/tour/infant-toddler05.jpg)" rel="it"></a>
+    <a href="images/tour/infant-toddler06.jpg" class="swipebox" style="background-image: url(images/tour/infant-toddler06.jpg)" rel="it"></a>
+    <a href="images/tour/infant-toddler07.jpg" class="swipebox" style="background-image: url(images/tour/infant-toddler07.jpg)" rel="it"></a>
+    <a href="images/tour/infant-toddler08.jpg" class="swipebox" style="background-image: url(images/tour/infant-toddler08.jpg)" rel="it"></a>
+    <a href="images/tour/infant-toddler09.jpg" class="swipebox" style="background-image: url(images/tour/infant-toddler09.jpg)" rel="it"></a>
+    <a href="images/tour/infant-toddler10.jpg" class="swipebox" style="background-image: url(images/tour/infant-toddler10.jpg)" rel="it"></a>
+    <a href="images/tour/infant-toddler11.jpg" class="swipebox" style="background-image: url(images/tour/infant-toddler11.jpg)" rel="it"></a>
+    <a href="images/tour/infant-toddler12.jpg" class="swipebox" style="background-image: url(images/tour/infant-toddler12.jpg)" rel="it"></a>
+
+    <div style="clear: both;"></div>
+  </div>
+  
+  <br><br>
+  <hr><br>
+  <br><br>
+
+  <h3 id="pssa">PRE-SCHOOL &amp; SCHOOL-AGE AREA</h3>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
+  <br>
+
+  <div class="video" data-id="T3s7bcOmHpg"></div>
+
+  <div class="gallery">
+    <a href="images/tour/pssa01.jpg" class="swipebox" style="background-image: url(images/tour/pssa01.jpg)" rel="pssa"></a>
+    <a href="images/tour/pssa02.jpg" class="swipebox" style="background-image: url(images/tour/pssa02.jpg)" rel="pssa"></a>
+    <a href="images/tour/pssa03.jpg" class="swipebox" style="background-image: url(images/tour/pssa03.jpg)" rel="pssa"></a>
+    <a href="images/tour/pssa04.jpg" class="swipebox" style="background-image: url(images/tour/pssa04.jpg)" rel="pssa"></a>
+    <a href="images/tour/pssa05.jpg" class="swipebox" style="background-image: url(images/tour/pssa05.jpg)" rel="pssa"></a>
+    <a href="images/tour/pssa06.jpg" class="swipebox" style="background-image: url(images/tour/pssa06.jpg)" rel="pssa"></a>
+    <a href="images/tour/pssa07.jpg" class="swipebox" style="background-image: url(images/tour/pssa07.jpg)" rel="pssa"></a>
+    <a href="images/tour/pssa08.jpg" class="swipebox" style="background-image: url(images/tour/pssa08.jpg)" rel="pssa"></a>
+    <a href="images/tour/pssa09.jpg" class="swipebox" style="background-image: url(images/tour/pssa09.jpg)" rel="pssa"></a>
+    <a href="images/tour/pssa10.jpg" class="swipebox" style="background-image: url(images/tour/pssa10.jpg)" rel="pssa"></a>
+    <a href="images/tour/pssa11.jpg" class="swipebox" style="background-image: url(images/tour/pssa11.jpg)" rel="pssa"></a>
+    <a href="images/tour/pssa12.jpg" class="swipebox" style="background-image: url(images/tour/pssa12.jpg)" rel="pssa"></a>
+
+    <div style="clear: both;"></div>
+  </div>
+
+  <br><br>
+  <hr><br>
+  <br><br>
+
+  <h3 id="outdoor">OUTDOOR AREA &amp; FACILITY</h3>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
+  <br>
+
+  <div class="video" data-id="T3s7bcOmHpg"></div>
+
+  <div class="gallery">
+    <a href="images/tour/outdoor01.jpg" class="swipebox" style="background-image: url(images/tour/outdoor01.jpg)" rel="outdoor"></a>
+    <a href="images/tour/outdoor02.jpg" class="swipebox" style="background-image: url(images/tour/outdoor02.jpg)" rel="outdoor"></a>
+    <a href="images/tour/outdoor03.jpg" class="swipebox" style="background-image: url(images/tour/outdoor03.jpg)" rel="outdoor"></a>
+    <a href="images/tour/outdoor04.jpg" class="swipebox" style="background-image: url(images/tour/outdoor04.jpg)" rel="outdoor"></a>
+    <a href="images/tour/outdoor05.jpg" class="swipebox" style="background-image: url(images/tour/outdoor05.jpg)" rel="outdoor"></a>
+    <a href="images/tour/outdoor06.jpg" class="swipebox" style="background-image: url(images/tour/outdoor06.jpg)" rel="outdoor"></a>
+    <a href="images/tour/outdoor07.jpg" class="swipebox" style="background-image: url(images/tour/outdoor07.jpg)" rel="outdoor"></a>
+    <a href="images/tour/outdoor08.jpg" class="swipebox" style="background-image: url(images/tour/outdoor08.jpg)" rel="outdoor"></a>
+
+    <div style="clear: both;"></div>
+  </div>
+
+  <script src="inc/youtube-play-button.js"></script>
+  <link rel="stylesheet" href="inc/swipebox/swipebox.css">
+  <script src="inc/swipebox/jquery.swipebox.min.js"></script>
+  <script type="text/javascript">$(document).ready(function() { $('.swipebox').swipebox(); });</script>
 </div>
 
 <div class="tour-footer-banner">
