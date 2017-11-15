cf0814e79cd1433ecc3b685210af310343ab06dd
foresitegroup <mark@foresitegrp.com>
2017-11-15 16:48:06 -0600

    Change email addresses

--- form-contact.php
+++ form-contact.php
@@ -13,7 +13,8 @@ if ($_POST['confirmationCAP'] == "") {
      )
   {
     $Subject = "Contact From Park's Edge Preschool Website";
-    $SendTo = "info@parksedgepreschool.com";
+    // $SendTo = "info@parksedgepreschool.com";
+    $SendTo = "rhianna@parksedgepreschool.com,kristin@parksedgepreschool.com,ellen@parksedgepreschool.com";
     $Headers = "From: Contact Form <parksedgepreschool@gmail.com>\r\n";
     $Headers .= "Reply-To: " . $_POST[md5('email' . $_POST['ip'] . $salt . $_POST['timestamp'])] . "\r\n";
     $Headers .= "Bcc: mark@foresitegrp.com\r\n";
