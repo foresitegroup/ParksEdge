1baaee7f618ad928181b87435dcbba293e438e48
foresitegroup <mark@foresitegrp.com>
2022-08-16 10:39:52 -0500

    Add new analytics tag

--- header.php
+++ header.php
@@ -43,15 +43,24 @@ if (!isset($TopDir)) $TopDir = "";
         });
       });
     </script>
-
+    
+    <!-- OLD Google Analytics tag - THIS WILL STOP WORKING JULY 1, 2023 -->
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
       ga('create', 'UA-89317864-1', 'auto');
       ga('send', 'pageview');
+    </script>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSND0QMJEY"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-PSND0QMJEY');
     </script>
   </head>
   <body>
